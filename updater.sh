27ec7ca933d52e005f5b7a3eb1778f3023e49ca6
TekLab <teklab@noreply.gitgem.com>
2018-06-16 23:01:36 +0200

    'updater.sh' ändern

--- updater.sh
+++ updater.sh
@@ -5,6 +5,14 @@ VAR_B=$2
 VAR_C=$3
 VAR_D=$4
 
+function getJSONData {
+        echo $1 | egrep -o "\"$2\": ?[^\}]*(\}|\")" | sed "s/\"$2\"://"
+}
+
+function getJSONVal {
+       echo $1 | egrep -o "\"$2\": ?\"[^\"]*" | sed "s/\"$2\"://" | tr -d '{}" '
+}
+
 if [ "$VAR_A" == "file" ]; then
 	wget --no-check-certificate $VAR_B/$VAR_C.tar
 	tar -xf $VAR_C.tar
@@ -17,7 +25,6 @@ if [ "$VAR_A" == "steam" ]; then
     http://media.steampowered.com/client/steamcmd_linux.tar.gz
 	tar -xzf steamcmd_linux.tar.gz
 	./steamcmd.sh +login $VAR_B +force_install_dir ./$VAR_D +app_update $VAR_C validate +exit
-	sleep 10
 	rm -rf steamcmd_linux.tar.gz
 	rm -rf steamcmd.sh
 	rm -rf steam.sh
@@ -25,8 +32,22 @@ if [ "$VAR_A" == "steam" ]; then
 fi
 
 if [ "$VAR_A" == "www" ]; then
-	# Next time
-	sleep 10
+	if [ "$VAR_B" == "minecraft" ]; then
+     VERSIONS=$(curl -s https://launchermeta.mojang.com/mc/game/version_manifest.json)
+	 LATEST_VER=$(getJSONVal "$(getJSONData "$VERSIONS" latest)" release)
+	 LATEST_DATA=$(echo $VERSIONS | egrep -o "\"id\":\"${LATEST_VER}\"[^}]*")
+	 LATEST_URL=$(getJSONVal "$LATEST_DATA" url)
+     CURL_JSON=$(curl -s $LATEST_URL)
+	 SERVER_FILE=minecraft_server_${LATEST_VER}.jar
+     # DEBUG CHECK START
+     echo "$VERSIONS"
+     echo "$LATEST_VER"
+     echo "$LATEST_DATA"
+     echo "$LATEST_URL"
+     echo "$CURL_JSON"
+     echo "$SERVER_FILE"
+     # DEBUG CHECK END
+    fi
 fi
 
 rm -rf updater.sh
