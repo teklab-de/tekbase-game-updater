e7cbd400a43c151ca417748354624a1191f78b50
sonix <sonix@noreply.gitgem.com>
2018-06-17 05:29:18 +0200

    'updater.sh' ändern

--- updater.sh
+++ updater.sh
@@ -5,6 +5,31 @@ VAR_B=$2
 VAR_C=$3
 VAR_D=$4
 
+DATE=$(date +"%d.%m.%Y_%H:%M:%S")
+LOGDIR="Logs"
+VERSION="mc_version.txt"
+VERSIONTEXT="1.1.0"
+
+if [[ -a "$LOGDIR" ]]; then
+     echo "Info: Ordner Logs für Logs vorhanden !"
+          
+else
+   mkdir $LOGDIR
+   chmod 755 $LOGDIR    
+   echo -n $DATE - Info: Ordner Logs nicht vorhanden wurde soeben erstellt ! >> $LOGDIR/$DATE-mc_up.log
+   echo $DATE - Info: Ordner Logs nicht vorhanden wurde soeben erstellt !
+fi
+
+if [[ -f "$VERSION" ]]; then
+        echo "Info: Die Datei mc_version.txt ist vorhanden !"
+else
+        echo -n ${DATE} - Info: Die Datei mc_version.txt wurde soeben erstellt ! >> $LOGDIR/$DATE-mc_up.log
+        echo ${DATE} - Info: Die Datei mc_version.txt wurde soeben erstellt !
+        touch $VERSION
+        echo $VERSIONTEXT > $VERSION   
+fi
+
+
 function getJSONData {
 	echo $1 | egrep -o "\"$2\": ?[^\}]*(\}|\")" | sed "s/\"$2\"://"
 }
@@ -31,12 +56,46 @@ fi
 
 if [ "$VAR_A" == "www" ]; then
 	if [ "$VAR_B" == "minecraft" ]; then
-		VERSIONS=$(curl -s https://launchermeta.mojang.com/mc/game/version_manifest.json)
-		GET_VER=$(getJSONVal "$(getJSONData "$VERSIONS" latest)" release)
-		GET_DATA=$(echo $VERSIONS | egrep -o "\"id\":\"${GET_VER}\"[^}]*")
-		GET_URL=$(getJSONVal "$GET_DATA" url)
-		GET_JSON=$(curl -s $GET_URL)
-		SERVER_FILE=minecraft_server_${GET_VER}.jar
+	     MCJSON=$(curl -s $__MC_JSON_URL)
+         LATEST_VER=$(getJSONVal "$(getJSONData "$MCJSON" latest)" release)
+         LATEST_URL_DATA=$(echo "$MCJSON" | egrep -o "\"id\":\"${LATEST_VER}\"[^}]*")
+         LATEST_VER_URL=$(getJSONVal "$LATEST_URL_DATA" url)
+         MCURLJSON=$(curl -s "$LATEST_VER_URL")
+         SERVER_JAR_NAME=minecraft_server_${LATEST_VER}.jar
+         SERVER_URL=$(getJSONVal "$(getJSONData "$MCURLJSON" server)" url)
+         SERVER_FILE_SHA1=$(getJSONVal "$(getJSONData "$MCURLJSON" server)" sha1)
+         LOCAL_FILE=$([[ -n $1 ]] && echo "$1" || echo "$SERVER_JAR_NAME")
+         SERVER_WGET=$(wget -q -O ${LOCAL_FILE} ${SERVER_URL})
+
+if [ ${LATEST_VER} != $(cat mc_version.txt) ]; then
+	echo $NEWVERSION > version.txt
+	$SERVER_WGET
+        echo -n ${DATE} - Die neueste Minecraft Version wurde gedownloaded >> $LOGDIR/$DATE-mc_up.log
+        echo ${DATE} - Die neueste Minecraft Version wurde gedownloaded
+else
+	echo "Info: Latest Version kein Update nötig !" 
+	exit
+fi
+
+
+if [ $(sha1sum "${LOCAL_FILE}" | awk '{print $1}') != "${SERVER_FILE_SHA1}" ]; then
+	echo "SHA1 of downloaded file does not match!" 
+	rm "${LOCAL_FILE}"
+else
+	echo -n '${DATE} - INFO: SHA1 matches!' >> $LOGDIR/$DATE-mc_up.log
+        echo ${DATE} - INFO: SHA1 matches!
+	
+fi
+
+if [[ -a "${LOCAL_FILE}" ]]; then
+	echo "${LOCAL_FILE} gedownloaded umbennen in minecraft_server.jar"
+	mv "${LOCAL_FILE}" "minecraft_server.jar"
+	echo -n ${DATE} - Datei $SERVER_JAR_NAME in minecraft_server.jar umbenannt! >> $LOGDIR/$DATE-mc_up.log
+	echo ${LATEST_VER} > mc_version.txt
+	echo -n ${DATE} Neue Version in mc_version.txt gespeichert! >> $LOGDIR/$DATE-mc_up.log
+
+fi
+
 		# DEBUG CHECK START
 		echo "$VERSIONS"
 		echo "$GET_VER"
