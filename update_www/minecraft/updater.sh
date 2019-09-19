1fa0210f4506d3637735a18a4b94c2eaa7f91289
Ch.Frankenstein <ch.frankenstein@googlemail.com>
2019-09-19 16:53:45 +0200

    Update updater.sh

--- update_www/minecraft/updater.sh
+++ update_www/minecraft/updater.sh
@@ -32,20 +32,27 @@ LOCALFILE=$([[ -n $1 ]] && echo "$1" || echo "$SERVERFILE")
 if [ "$LATESTVER" != "$(cat $VAR_A/version.tek)" ]; then
     echo $GETVER > $VAR_A/version.tek
     wget -q -O $LOCALFILE $SERVERURL
-    echo "$(date) - The latest version has been downloaded!" >> $VAR_A/logs/$LOGDATE-update.log
+    echo "$(date) - INFO: The latest version has been downloaded!" >> $VAR_A/logs/$LOGDATE-update.log
+
+    if [ "$(sha1sum $LOCALFILE | awk '{print $1}')" != "$SERVERFILESHA1" ]; then
+        echo "$(date) - Checksum of downloaded file does not match!" >> $VAR_A/logs/$LOGDATE-update.log
+        rm "$LOCALFILE"
+    fi
+
+    if [ -f $LOCAL_FILE ]; then
+        mv $VAR_A/minecraft_server.jar $VAR_A/${LOGDATE}_minecraft_server.jar
+        mv "$LOCALFILE" $VAR_A/minecraft_server.jar
+        echo "$(date) - INFO: The update was successful!" >> $VAR_A/logs/$LOGDATE-update.log
+    fi
+else
+	echo "$(date) - INFO: The latest version is already installed!" >> $LOGDIR/$LOGFILE-update.log
 fi
 
-if [ "$(sha1sum $LOCALFILE | awk '{print $1}')" != "$SERVERFILESHA1" ]; then
-    echo "$(date) - Checksum of downloaded file does not match!" >> $VAR_A/logs/$LOGDATE-update.log
-    rm "$LOCALFILE"
-fi
+echo "$(date) - DEBUG" >> $VAR_A/logs/$LOGDATE-update.log
+echo "Latest version=$GETVER" >> $VAR_A/logs/$LOGDATE-update.log
+echo "Data request=$GETDATA" >> $VAR_A/logs/$LOGDATE-update.log
+echo "Version URL=$GETURL" >> $VAR_A/logs/$LOGDATE-update.log
+echo "Download URL=$SERVERURL" >> $VAR_A/logs/$LOGDATE-update.log
 
-if [ -f $LOCAL_FILE ]; then
-    mv $VAR_A/minecraft_server.jar $VAR_A/${LOGDATE}_minecraft_server.jar
-    mv "$LOCALFILE" $VAR_A/minecraft_server.jar
-    echo "$(date) - The update was successful!" >> $VAR_A/logs/$LOGDATE-update.log
-fi
-
-echo "$(date) - Latest version: $GETVER, Data request: $GETDATA, Version URL: $GETURL, Download URL: $SERVERURL" >> $VAR_A/logs/$LOGDATE-update.log
 
 exit 0
