fe366a34c9a6f4f982f95edf08bd40558057f25d
IllegalMex <99988436+IllegalMex@users.noreply.github.com>
2022-02-18 18:45:56 +0100

    Update updater.sh
        Programmcode größtenteils neu erstellt.
        Detaillierte Informationen für /log/ inklusive Debug-Infos vorhanden.
        Aktuelle Minecraft Serverversion wird in version.tek abgespeichert und bei einem erneuten Updatevorgang abgefragt. Ist der Gameserver aktuell wird der Updatevorgang ohne Übertragung weiter Daten beendet.
        Integrität der heruntergeladenen Datei wird mittels SHA1 Checksumme überprüft.
        Die veraltete minecraft_server.jar wird archiviert.

--- update_www/minecraft/updater.sh
+++ update_www/minecraft/updater.sh
@@ -1,58 +1,47 @@
 #!/bin/bash
 
-# TekBase - server control panel
-# Copyright since 2005 TekLab
-# Christian Frankenstein
-# Website: teklab.de
-#          teklab.net
-
 VAR_A=$1
-
 LOGDATE=$(date +"%Y-%m-%d")
+LOGFILE=$(date +"%Y-%m")
+LOGDIR="logs"
 
-function getJSONData {
-    echo $1 | grep -E -o "\"$2\": ?[^\}]*(\}|\")" | sed "s/\"$2\"://"
-}
+readonly MINECRAFT_DIR=$VAR_A
+readonly MINECRAFT_SERVER_LATEST_VER=$( curl https://launchermeta.mojang.com/mc/game/version_manifest.json|jq -r '."latest"."release"' )
+readonly MINECRAFT_SERVER_LATEST_MANIFEST_URL=$( curl https://launchermeta.mojang.com/mc/game/version_manifest.json | jq -r ".\"versions\"[] | select (.\"id\"==\"${MINECRAFT_SERVER_LATEST_VER}\") | .\"url\" " )
+readonly MINECRAFT_SERVER_LATEST_JAR_URL=$( curl "${MINECRAFT_SERVER_LATEST_MANIFEST_URL}" | jq -r ".\"downloads\".\"server\".\"url\"" )
+readonly MINECRAFT_SERVER_LATEST_JAR_SHA1=$( curl "${MINECRAFT_SERVER_LATEST_MANIFEST_URL}" | jq -r ".\"downloads\".\"server\".\"sha1\"" )
+readonly SERVERFILE="minecraft_server.${MINECRAFT_SERVER_LATEST_VER}.jar"
+# readonly LOCALFILE=$([[ -n $1 ]] && echo "$1" || echo "$SERVERFILE")
 
-function getJSONVal {
-    echo $1 | grep -E -o "\"$2\": ?\"[^\"]*" | sed "s/\"$2\"://" | tr -d '{}" '
-}
+# readonly MINECRAFT_SERVER_CURRENT_INSTALLED_VER=$( ls ${MINECRAFT_DIR}/*.jar | cut -d '/' -f4 | sed -e "s/minecraft_server\.//" -e "s/\.jar//" )
+# rm ${MINECRAFT_DIR}/minecraft_server.${MINECRAFT_SERVER_CURRENT_INSTALLED_VER}.jar
 
+if [ "$MINECRAFT_SERVER_LATEST_VER" != "$(cat $VAR_A/version.tek)" ]; then
+	wget ${MINECRAFT_SERVER_LATEST_JAR_URL} -O ${MINECRAFT_DIR}/minecraft_server.${MINECRAFT_SERVER_LATEST_VER}.jar
+	echo "$(date) - INFO: The latest server version has been downloaded!" >> $VAR_A/$LOGDIR/$LOGFILE-update.log
 
-VERSIONS=$(curl -s https://launchermeta.mojang.com/mc/game/version_manifest.json)
-GETVER=$(getJSONVal "$(getJSONData "$VERSIONS" "latest")" "release")
-GETDATA=$(echo "$VERSIONS" | grep -E -o "\"id\":\"$GETVER\"[^}]*")
-GETURL=$(getJSONVal "$GETDATA" "url")
-GETJSON=$(curl -s $GETURL)
-SERVERFILE="minecraft_server_${GETVER}.jar"
-SERVERFILESHA1=$(getJSONVal "$(getJSONData "$GETJSON" "server")" "sha1")
-SERVERURL=$(getJSONVal "$(getJSONData "$GETJSON" "server")" "url")        
-LOCALFILE=$([[ -n $1 ]] && echo "$1" || echo "$SERVERFILE")
+	if [ "$(sha1sum $SERVERFILE | awk '{print $1}')" != "$MINECRAFT_SERVER_LATEST_JAR_SHA1" ]; then
+        	echo "$(date) - ERROR: Checksum of downloaded file does not match! Aborting gameserver update!" >> $VAR_A/$LOGDIR/$LOGFILE-update.log
+		rm "$SERVERFILE"
+		exit 0
+	else
+		echo "$(date) - INFO: Checksum of downloaded file ok!" >> $VAR_A/$LOGDIR/$LOGFILE-update.log
+	fi
 
-if [ "$LATESTVER" != "$(cat $VAR_A/version.tek)" ]; then
-    echo $GETVER > $VAR_A/version.tek
-    wget -q -O $LOCALFILE $SERVERURL
-    echo "$(date) - INFO: The latest version has been downloaded!" >> $VAR_A/logs/$LOGDATE-update.log
-
-    if [ "$(sha1sum $LOCALFILE | awk '{print $1}')" != "$SERVERFILESHA1" ]; then
-        echo "$(date) - Checksum of downloaded file does not match!" >> $VAR_A/logs/$LOGDATE-update.log
-        rm "$LOCALFILE"
-    fi
-
-    if [ -f $LOCAL_FILE ]; then
-        mv $VAR_A/minecraft_server.jar $VAR_A/${LOGDATE}_minecraft_server.jar
-        mv "$LOCALFILE" $VAR_A/minecraft_server.jar
-        echo "$(date) - INFO: The update was successful!" >> $VAR_A/logs/$LOGDATE-update.log
-    fi
+	mv ${MINECRAFT_DIR}/minecraft_server.jar ${MINECRAFT_DIR}/${LOGDATE}_minecraft_server.jar
+	mv ${SERVERFILE} ${MINECRAFT_DIR}/minecraft_server.jar
+	echo "$MINECRAFT_SERVER_LATEST_VER" > version.tek   
+	echo "$(date) - INFO: version.tek has been modified to latest release version!" >> $VAR_A/$LOGDIR/$LOGFILE-update.log
+	echo "$(date) - INFO: The server was successful updated to version $MINECRAFT_SERVER_LATEST_VER!" >> $VAR_A/$LOGDIR/$LOGFILE-update.log
+	echo "$(date) - DEBUG: Latest version=$MINECRAFT_SERVER_LATEST_VER" >> $VAR_A/$LOGDIR/$LOGFILE-update.log
+	echo "$(date) - DEBUG: Manifest url=$MINECRAFT_SERVER_LATEST_MANIFEST_URL" >> $VAR_A/$LOGDIR/$LOGFILE-update.log
+	echo "$(date) - DEBUG: Server jar url=$MINECRAFT_SERVER_LATEST_JAR_URL" >> $VAR_A/$LOGDIR/$LOGFILE-update.log
+	echo "$(date) - DEBUG: Server file name=$SERVERFILE" >> $VAR_A/$LOGDIR/$LOGFILE-update.log
+	echo "$(date) - DEBUG: SHA1 Checksum=$MINECRAFT_SERVER_LATEST_JAR_SHA1" >> $VAR_A/$LOGDIR/$LOGFILE-update.log
 else
-	echo "$(date) - INFO: The latest version is already installed!" >> $LOGDIR/$LOGFILE-update.log
-fi
+	echo "$(date) - INFO: The latest version $MINECRAFT_SERVER_LATEST_VER is already installed! No need to update!" >> $VAR_A/$LOGDIR/$LOGFILE-update.log
 
-echo "$(date) - DEBUG" >> $VAR_A/logs/$LOGDATE-update.log
-echo "Latest version=$GETVER" >> $VAR_A/logs/$LOGDATE-update.log
-echo "Data request=$GETDATA" >> $VAR_A/logs/$LOGDATE-update.log
-echo "Version URL=$GETURL" >> $VAR_A/logs/$LOGDATE-update.log
-echo "Download URL=$SERVERURL" >> $VAR_A/logs/$LOGDATE-update.log
+fi
 
 
 exit 0
